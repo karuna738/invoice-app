72cd64095b8ec0bdf0c476edf1f1de7093a3e5e1
karuna738 <karankarna738@gmail.com>
2025-09-08 15:53:58 +0530

    sep-08

--- frontend/src/app/components/components-routing.module.ts
+++ frontend/src/app/components/components-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-   { path: '', redirectTo: '/invoices', pathMatch: 'full' },
+   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   {
+    path: 'dashboard',
+    component: DashboardComponent
+   },
   {
     path: 'invoices',
     loadChildren: () => import('./invoice/invoice.module').then(m => m.InvoiceModule)
